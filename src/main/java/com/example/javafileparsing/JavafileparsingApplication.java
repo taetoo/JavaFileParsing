56cd94acf62dc7bcd958b4ef3fac7a0d938f5961
taetoo <rlaougs5@naver.com>
2022-08-22 08:33:56 +0900

    Initial commit

---
+++ src/main/java/com/example/javafileparsing/JavafileparsingApplication.java
@@ -0,0 +1,13 @@
+package com.example.javafileparsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafileparsingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavafileparsingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javafileparsing/JavafileparsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javafileparsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafileparsingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
